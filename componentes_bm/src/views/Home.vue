dcfd711abe60b67806f6bc1c7f9ac1ec32be0c98
Marcos Santos <msantos@mem.gob.gt>
2022-12-18 20:01:46 -0600

    [FIX] cambio de componente

--- componentes_bm/src/views/Home.vue
+++ componentes_bm/src/views/Home.vue
@@ -1,11 +1,19 @@
 <template>
   <v-app>
     <v-main>
-    <div class="home">
-      <img alt="Vue logo" src="../assets/logo.png">
-      <HelloWorld msg="Welcome to Your Vue.js App"/>
-      <customTable :columns="columnas" :rows="filas" :icon_edit="'edit'" :titleTable="'Titulo de la tabla'" :item="item"></customTable>
-    </div>
+      <div class="home">
+        <img alt="Vue logo" src="../assets/logo.png">
+        <HelloWorld msg="Welcome to Your Vue.js App" />
+        <v-card>
+          <v-data-table :headers="columnas_t" :items="filas_t" :search="search">
+            <template v-slot:top>
+              <customTable @find="find" :size_letter_title="30" :size_letter_data="10" :search="search" :columns="columnas_pdf" :rows="filas_t" :titleTable="'Titulo de la tabla'"></customTable>
+            </template>
+
+          </v-data-table>
+        </v-card>
+
+      </div>
     </v-main>
   </v-app>
 </template>
@@ -18,30 +26,45 @@ import { customTable } from "vuetify-more-component";
 export default {
   name: 'Home',
   components: {
-    HelloWorld,customTable
+    HelloWorld, customTable
   },
 
   data() {
     return {
 
       loading: false,
-      columnas: 
-      [
-        {text: "descripcion" , value:"descripcion"},
-        {text: "acciones" ,value:"actions"}
+      columnas_t:
+        [     
+        {  text: "id", value: "id", align:"center"},
+        { text: "descripcion", value: "descripcion", align:"center" },
+
+        ],
+
+      filas_t: [
+        { id: 1, descripcion: "item 1" },
+        { id: 2, descripcion: "item 2" },
+        { id: 3, descripcion: "item 3" },
+        { id: 4, descripcion: "ITEM 3" },
+        { id: 5, descripcion: "ÍTEM 3" },
+        { id: 6, descripcion: "ÍTEM" },
+        { id: 7, descripcion: "ÍTEM3" },
       ],
 
-      filas: [
-        {id:1,descripcion:"item 1"},
-        {id:2,descripcion:"item 2"},
-        {id:3,descripcion:"item 3"},
-      ],
+      columnas_pdf:
+        [     
+        "ID","DESCRIPCION"
+        ],
 
-      item:{
-        descripcion: "",
-      }
+      search: "",
 
     };
+    
   },
+
+  methods:{
+    find(data){
+      this.search = data;
+    }
+  }
 }
 </script>
